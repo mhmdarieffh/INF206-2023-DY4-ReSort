417d497e5f65d9e67a10c78569200caa334afdbe
elsahariskaa <elsa_ma@mhs.unsyiah.ac.id>
2023-04-28 00:06:15 +0700

    feat: mengganti kode css nya ke tailwind

--- resources/views/berandapetugas.blade.php
+++ resources/views/berandapetugas.blade.php
@@ -3,66 +3,97 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=auto, height=auto">
-    <title>ReSort</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Form Petugas</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@400;500;700;800;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="public/css/formpetugas.css">
+    <link rel="stylesheet" href="formpetugas.css">
 </head>
 <body>
-    <header style="width: 1482px; height: 117px; background-color: #e6e6e6; border-radius: 30px;">
-        <img src="{{asset('assets/img/logoresort.png')}}" alt="icon" style="margin-left: 45px; margin-top: 39px; width: 212px; height: 39px;"/>
-        <nav style="margin-top: 40px; font-size: 30px;">
-            <a href="berandapetugas.php" class="beranda">Beranda</a>
-            <a href="" class="kontak">Kontak</a>
-            <a href="" class="halo">Halo,Zul</a>
-        </nav>
-    </header>
-    <table style="font-size: 36px; margin-left: 45px; margin-top: 75px; margin-bottom: 120px;">
+<div class="bg-[#ADDE86] p-8">
+    <div class="bg-[#D9D9D9] rounded-[20px]"> 
+      <div class="bg-[#E6E6E6]  rounded-[20px]">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between h-16 " >
+            <!-- Logo -->
+            <div class="flex items-center justify-center">
+              <img class="h-8 w-auto" src="logoresort.png" alt="Logo" />
+            </div>
+
+            <!-- Menu -->
+            <div class="flex justify-between items-center text-[20px] p-[20px]">
+              <!-- Beranda -->
+              <a
+                href="#"
+                class="px-3 py-2 text-gray-800 hover:text-white font-semibold"
+                >Beranda</a
+              >
+
+              <!-- Kontak -->
+              <a
+                href="#"
+                class="px-3 py-2 text-gray-800 hover:text-white font-semibold"
+                >Kontak</a
+              >
+
+              <!-- Logout -->
+              <a
+                href="#"
+                class="px-3 py-2 text-gray-800 hover:text-white font-semibold"
+                >Logout</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="p-[30px]">
+    </div>
+    <table class="text-3xl ml-10 mt-8 mb-24">
         <tr>
             <td>Nama</td>
-            <td style="padding-right: 5px;">:</td>
+            <td class="pr-5">:</td>
             <td>Zul</td>
         </tr>
         <tr>
-            <td style="padding-right: 25px;">ID Petugas</td>
+            <td class="pr-25">ID Petugas</td>
             <td>:</td>
             <td>00154</td>
         </tr>
     </table>
-    <div style="width: 1482px; max-height: auto; margin-bottom: 120px;">
-        <table class="tabel" style="margin-bottom: 10px;">
-            <thead style="background-color: #adde86; font-size: 30px; text-align: center;">
+        <table class="mx-auto justify-center items-center border-collapse w-93 mb-50">
+            <thead class="bg-[#adde86] text-2xl text-center">
                 <tr>
-                    <td class="no">NO</td>
-                    <td class="nama">NAMA</td>
-                    <td class="alamat">ALAMAT</td>
-                    <td class="status">STATUS</td>
+                    <td class="border-2 border-solid border-black pl-6 pr-6">NO</td>
+                    <td class="border-2 border-solid border-black pl-20 pr-20">NAMA</td>
+                    <td class="border-2 border-solid border-black pl-40 pr-40">ALAMAT</td>
+                    <td class="border-2 border-solid border-black pl-16 pr-16">STATUS</td>
                 </tr>
             </thead>
-        </table>
-        <table class="tabel">
-            <tbody style="background-color: white; font-size: 30px; text-align: center;">
+            <tbody class="bg-[#ffffff] text-2xl text-center">
                 <tr>
-                    <td class="no">1</td>
-                    <td class="nama">Irma</td>
-                    <td class="alamat">Jl.Ikan</td>
-                    <td class="status">SUDAH</td>
+                    <td class="border-2 border-solid border-black pl-6 pr-6">1</td>
+                    <td class="border-2 border-solid border-black pl-20 pr-20">Irma</td>
+                    <td class="border-2 border-solid border-black pl-40 pr-40">Jl.Ikan</td>
+                    <td class="border-2 border-solid border-black pl-16 pr-16">SUDAH</td>
                 </tr>
                 <tr>
-                    <td class="no">2</td>
-                    <td class="nama">Adi</td>
-                    <td class="alamat">Jl.Ayam</td>
-                    <td class="status">SUDAH</td>
+                    <td class="border-2 border-solid border-black pl-6 pr-6">2</td>
+                    <td class="border-2 border-solid border-black pl-20 pr-20">Adi</td>
+                    <td class="border-2 border-solid border-black pl-40 pr-40">Jl.Ayam</td>
+                    <td class="border-2 border-solid border-black pl-16 pr-16">SUDAH</td>
                 </tr>
                 <tr>
-                    <td class="no">3</td>
-                    <td class="nama">Imran</td>
-                    <td class="alamat">Jl.Layang</td>
-                    <td class="status">BELUM</td>
+                    <td class="border-2 border-solid border-black pl-6 pr-6">3</td>
+                    <td class="border-2 border-solid border-black pl-20 pr-20">Imran</td>
+                    <td class="border-2 border-solid border-black pl-40 pr-40">Jl.Layang</td>
+                    <td class="border-2 border-solid border-black pl-16 pr-16">BELUM</td>
                 </tr>
             </tbody>
         </table>
-    </div>
+        <div>
+          <br><br><br>
+        </div>
+</div>
 </body>
 </html>
