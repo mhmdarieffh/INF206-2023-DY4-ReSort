e7af15e8eb09b5635774f2987eb6d30c640bbc19
Putri Ulfayani <putriulfayani1608@gmail.com>
2023-05-10 13:12:51 +0700

    feat : add role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('role')-> default(0);
             $table->string('alamat');
             $table->string('no_hp');
             $table->timestamp('email_verified_at')->nullable();
