4f45b81001502d5f6cff185f8e561dd2e97aa19b
RayyanAzzuhri <rayzuhri07@gmail.com>
2023-05-12 11:42:13 +0700

    Fix: minor change in some page

--- resources/views/warga/ajuSuccess.blade.php
+++ resources/views/warga/ajuSuccess.blade.php
@@ -5,10 +5,10 @@
                 <div class="mx-auto px-4">
                     <div class="p-[30px]">
                         <div class="flex items-center justify-center bg-[#E6E6E6] rounded-[20px] h-screen">
-                            <div class="justify-center bg-[#ADDE86] w-[600px] h-[500px] p-10 rounded-[20px]">>
+                            <div class="justify-center bg-[#ADDE86] w-[600px] h-[500px] p-10 rounded-[20px]">
                                 <div class="flex items-center justify-center">
                                     <img class="h-[150px] w-auto mb-4" src="{{url('/images/Vector.png')}}"
-                                        alt="image" />
+                                        alt="image"/>
                                 </div>
                                 <h1 class="relative flex justify-center text-[25px] font-bold p-[10px] text-[#244454]">
                                     SUCCEED</h1>
